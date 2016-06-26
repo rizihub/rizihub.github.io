baf815bed3f711ba0c7b1210d2fe4d96f06149e6
rizihub <rizi.hub@gmail.com>
2016-06-26 19:29:11 +0100

    made sure navbar stays on top along with minor style changes

---
+++ scripts/master.js
@@ -0,0 +1,8 @@
+//Ensure all the content is below the navbar-nav
+function moveContent() {
+    var navbarHeight = document.getElementById('my-navbar').clientHeight;
+    var content = document.querySelector("#content");
+    content.style.marginTop = navbarHeight.toString() + "px";
+}
+moveContent();
+window.onresize = moveContent;
